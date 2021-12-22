46c7dde03b64796f0e610a6bd77cce105b22bb9f
Igor Shadurin <igor.shadurin@gmail.com>
2021-12-22 21:08:34 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// import { Bee } from './bee'
+// import { BeeDebug } from './bee-debug'
+//
+// export * as Utils from './utils/expose'
+// export * from './types'
+// export * from './utils/error'
+// export { SUPPORTED_BEE_VERSION, SUPPORTED_BEE_VERSION_EXACT } from './modules/debug/status'
+// export { Bee, BeeDebug }
+
+// for require-like imports
+declare global {
+  interface Window {
+    // bound as 'FairdriveProtocol' via Webpack
+    FairdriveProtocol: {
+      // Bee: typeof import('./bee').Bee
+      // BeeDebug: typeof import('./bee-debug').BeeDebug
+      // Utils: typeof import('./utils/expose')
+      // BeeError: typeof import('./utils/error').BeeError
+      // BeeRequestError: typeof import('./utils/error').BeeRequestError
+      // BeeResponseError: typeof import('./utils/error').BeeResponseError
+      // BeeArgumentError: typeof import('./utils/error').BeeArgumentError
+    }
+  }
+}
