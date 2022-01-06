2be9e68f64a0c19284474f11511645949a52325c
nugaon <toth.viktor.levente@gmail.com>
2022-01-06 18:03:55 +0100

    refactor: not used wallet class

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,5 +1,4 @@
 export * as Account from './account'
 export * as Encryption from './encryption'
 export * as Utils from './utils'
-export * as Wallet from './wallet'
 //TODO export every exportable
