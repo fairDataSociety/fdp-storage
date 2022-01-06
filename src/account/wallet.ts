cd664a9601a528ae1dea9bd887df8e262d6be011
nugaon <toth.viktor.levente@gmail.com>
2022-01-06 17:49:17 +0100

    style: wrong style format at imports

--- src/account/wallet.ts
+++ src/account/wallet.ts
@@ -1,8 +1,8 @@
-import {Data} from '@ethersphere/bee-js/dist/src/types'
-import {decrypt} from "./encryption";
-import {wrapBytesWithHelpers} from "../utils/bytes";
-import {sha3_256} from "js-sha3";
-import {hexToBytes} from "../utils/hex";
+import { Data } from '@ethersphere/bee-js/dist/src/types'
+import { sha3_256 } from 'js-sha3'
+import { wrapBytesWithHelpers } from '../utils/bytes'
+import { hexToBytes } from '../utils/hex'
+import { decrypt } from './encryption'
 
 export class Wallet {
   encryptedmnemonic: Data
