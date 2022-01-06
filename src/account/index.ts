feaa740c84cc2434e96cb980898569b96cc86b27
nugaon <toth.viktor.levente@gmail.com>
2022-01-06 17:32:50 +0100

    chore: add todo for account index exportables

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,4 +1,5 @@
+export * as Account from './account'
+export * as Encryption from './encryption'
 export * as Utils from './utils'
 export * as Wallet from './wallet'
-export * as Encryption from './encryption'
-export * as Account from './account'
+//TODO export every exportable
