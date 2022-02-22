e9e0731d45f1956e6b5321aca60932bcc55cd24e
IgorShadurin <igor.shadurin@gmail.com>
2022-02-22 16:25:15 +0300

    refactor: update src/account/batch.ts
    Co-authored-by: nugaon <50576770+nugaon@users.noreply.github.com>

--- src/account/batch.ts
+++ src/account/batch.ts
@@ -3,11 +3,11 @@ import { BeeDebug } from '@ethersphere/bee-js'
 export async function getBatchId(beeDebug: BeeDebug): Promise<string> {
   const batches = await beeDebug.getAllPostageBatch()
 
-  if (!batches.length) {
+  if (batches.length === 0) {
     throw new Error('Postage batch not exists')
   }
 
-  const batchId = batches?.pop()?.batchID
+  const batchId = batches.pop()?.batchID
 
   if (!batchId) {
     throw new Error('Incorrect batch id found')
