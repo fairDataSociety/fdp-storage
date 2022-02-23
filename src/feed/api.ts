d826b0addcf11dc35c26f31d9461db32023fdc1a
Igor Shadurin <igor.shadurin@gmail.com>
2022-02-24 00:01:01 +0300

    refactor: removed unused param

--- src/feed/api.ts
+++ src/feed/api.ts
@@ -7,18 +7,17 @@ import { keccak256Hash } from '../account/encryption'
 import { lookup } from './lookup/linear'
 import { Epoch } from './lookup/epoch'
 import Long from 'long'
-import { NoClue } from './lookup/lookup'
 
-const downloadTimeout = 1000
+const DOWNLOAD_TIMEOUT = 1000
 
 export async function getFeedData(bee: Bee, topic: string, address: string): Promise<Data> {
   const addressBytes = Utils.makeEthAddress(address)
   const topicHash = bmtHashString(topic)
-  const chunk = await lookup(Long.fromNumber(0), NoClue, async (epoch: Epoch, time: Long): Promise<Data> => {
+  const chunk = await lookup(Long.fromNumber(0), async (epoch: Epoch, time: Long): Promise<Data> => {
     const tempId = getId(topicHash, time.toNumber(), epoch.level)
     const chunkReference = bytesToHex(keccak256Hash(tempId, addressBytes))
 
-    return await bee.downloadChunk(chunkReference, { timeout: downloadTimeout })
+    return await bee.downloadChunk(chunkReference, { timeout: DOWNLOAD_TIMEOUT })
   })
 
   return extractChunkData(chunk)
