eb91408ec1e77447e50ff448d6acd9f77abc4efd
Igor Shadurin <igor.shadurin@gmail.com>
2022-03-05 22:51:09 +0400

    refactor: optimized constructor

--- src/account/account-data.ts
+++ src/account/account-data.ts
@@ -2,13 +2,5 @@ import { Bee, BeeDebug } from '@ethersphere/bee-js'
 import { Wallet } from 'ethers'
 
 export default class AccountData {
-  public readonly bee: Bee
-  public readonly beeDebug: BeeDebug
-  public wallet?: Wallet
-
-  constructor(bee: Bee, beeDebug: BeeDebug, wallet?: Wallet) {
-    this.bee = bee
-    this.beeDebug = beeDebug
-    this.wallet = wallet
-  }
+  constructor(public readonly bee: Bee, public readonly beeDebug: BeeDebug, public wallet?: Wallet) {}
 }
