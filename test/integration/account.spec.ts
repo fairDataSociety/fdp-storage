583e1eb8e47b08fb6a3c25c7423da5e712811811
Igor Shadurin <igor.shadurin@gmail.com>
2022-03-06 17:43:17 +0400

    refactor: test upgraded for bee-factory environment

--- test/integration/account.spec.ts
+++ test/integration/account.spec.ts
@@ -1,4 +1,5 @@
 import { FairdriveProtocol } from '../../src'
+import { generateUser } from '../utils'
 
 function createFdp() {
   return new FairdriveProtocol('http://localhost:1633/', 'http://localhost:1635/')
@@ -6,49 +7,68 @@ function createFdp() {
 
 jest.setTimeout(200000)
 describe('Account', () => {
+  const users = {
+    debug: generateUser(),
+    demo: generateUser(),
+  }
+
   it('should strip trailing slash', () => {
     const fdp = createFdp()
     expect(fdp.accountData.bee.url).toEqual('http://localhost:1633')
   })
 
+  describe('Registration', () => {
+    it('register required users', async () => {
+      const fdp = createFdp()
+      const { debug, demo } = users
+      let createdUser = await fdp.userSignup(debug.username, debug.password, debug.mnemonic)
+      expect(createdUser.mnemonic).toEqual(debug.mnemonic)
+      expect(createdUser.wallet.address).toEqual(debug.address)
+      expect(createdUser.encryptedMnemonic).toBeDefined()
+      expect(createdUser.reference).toBeDefined()
+
+      createdUser = await fdp.userSignup(demo.username, demo.password, demo.mnemonic)
+      expect(createdUser.mnemonic).toEqual(demo.mnemonic)
+      expect(createdUser.wallet.address).toEqual(demo.address)
+    })
+  })
+
   describe('Login', () => {
     it('should login with existing user and address', async () => {
       const fdp = createFdp()
+      const { debug, demo } = users
       expect(fdp.users.debug).toBeUndefined()
-      await fdp.userImport('debug', '0x1f8f8EC28a1ED657836ADB02bed12C78F05cC8Dc')
-      expect(fdp.users.debug).toBeDefined()
-      await fdp.userLogin('debug', 'debug')
+      await fdp.userImport(debug.username, debug.address)
+      expect(fdp.users[debug.username]).toBeDefined()
+      await fdp.userLogin(debug.username, debug.password)
 
       expect(fdp.users.demo).toBeUndefined()
-      await fdp.userImport('demo', '0x9E8A6709eD1E58FA2241E7ea70308A37bbEc3B7B')
-      expect(fdp.users.demo).toBeDefined()
-      await fdp.userLogin('demo', 'demo')
+      await fdp.userImport(demo.username, demo.address)
+      expect(fdp.users[demo.username]).toBeDefined()
+      await fdp.userLogin(demo.username, demo.password)
     })
 
     it('should login with existing user and mnemonic', async () => {
       const fdp = createFdp()
-
+      const { debug } = users
       expect(fdp.users.debug).toBeUndefined()
-      await fdp.userImport(
-        'debug',
-        '',
-        'never umbrella juice enable mask industry leopard media hybrid tornado wrong behave',
-      )
-      expect(fdp.users.debug).toBeDefined()
-      await fdp.userLogin('debug', 'debug')
+      await fdp.userImport(debug.username, '', debug.mnemonic)
+      expect(fdp.users[debug.username]).toBeDefined()
+      await fdp.userLogin(debug.username, debug.password)
     })
 
     it('auth with incorrect data should throw errors', async () => {
       const fdp = createFdp()
+      const { debug } = users
       // not imported user
       await expect(fdp.userLogin('zzz', 'zzz')).rejects.toThrow('User is not imported')
 
       // imported, but incorrect password
-      await fdp.userImport('debug', '0x1f8f8EC28a1ED657836ADB02bed12C78F05cC8Dc')
-      await expect(fdp.userLogin('debug', 'debug111')).rejects.toThrow('Incorrect password')
+      await fdp.userImport(debug.username, debug.address)
+      await expect(fdp.userLogin(debug.username, 'debug111')).rejects.toThrow('Incorrect password')
 
       // imported, but empty password
-      await expect(fdp.userLogin('debug', '')).rejects.toThrow('Incorrect password')
+      await expect(fdp.userLogin(debug.username, '')).rejects.toThrow('Incorrect password')
 
       // import with address and mnemonic
       await expect(
@@ -64,34 +84,16 @@ describe('Account', () => {
       // import without info
       await expect(fdp.userImport('', '', '')).rejects.toThrow('Incorrect username')
     })
+  })
 
-    it('signup', async () => {
+  describe('Pods', () => {
+    it('get empty pods list', async () => {
       const fdp = createFdp()
-      const user = {
-        username: 'test000000000',
-        password: 'aaa',
-      }
-
-      const userInfo = await fdp.userSignup(user.username, user.password)
-      expect(userInfo).toBeDefined()
-      expect(userInfo.wallet).toBeDefined()
-      expect(userInfo.mnemonic).toBeDefined()
-      expect(userInfo.encryptedMnemonic).toBeDefined()
-      expect(userInfo.reference).toBeDefined()
-      await fdp.userImport(user.username, userInfo.wallet.address)
-      await fdp.userLogin(user.username, user.password)
-    })
-
-    it('Pod ls', async () => {
-      const fdp = createFdp()
-      await fdp.userImport(
-        'debug',
-        '',
-        'never umbrella juice enable mask industry leopard media hybrid tornado wrong behave',
-      )
+      const { debug } = users
+      await fdp.userImport(debug.username, '', debug.mnemonic)
 
       const pods = await fdp.podLs()
-      expect(pods).toHaveLength(50)
+      expect(pods).toHaveLength(0)
     })
   })
 })
