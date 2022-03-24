9cd2a793c014f2e8dc4fbbd05d80321a33c68207
Igor Shadurin <igor.shadurin@gmail.com>
2022-03-24 12:58:44 +0400

    refactor: constant converted to optional parameter

--- src/feed/api.ts
+++ src/feed/api.ts
@@ -5,23 +5,22 @@ import { lookup } from './lookup/linear'
 import { Epoch } from './lookup/epoch'
 import { bytesToHex } from '../utils/hex'
 
-const DOWNLOAD_TIMEOUT = 1000
-
 /**
  * Finds and downloads the latest feed content
  *
  * @param bee Bee client
  * @param topic topic for calculation swarm chunk
  * @param address Ethereum address for calculation swarm chunk
+ * @param timeout download timeout during finding
  */
-export async function getFeedData(bee: Bee, topic: string, address: string): Promise<Data> {
+export async function getFeedData(bee: Bee, topic: string, address: string, timeout = 1000): Promise<Data> {
   const addressBytes = Utils.makeEthAddress(address)
   const topicHash = bmtHashString(topic)
   const chunk = await lookup(0, async (epoch: Epoch, time: number): Promise<Data> => {
     const tempId = getId(topicHash, time, epoch.level)
     const chunkReference = bytesToHex(Utils.keccak256Hash(tempId, addressBytes))
 
-    return await bee.downloadChunk(chunkReference, { timeout: DOWNLOAD_TIMEOUT })
+    return await bee.downloadChunk(chunkReference, { timeout })
   })
 
   return extractChunkContent(chunk)
