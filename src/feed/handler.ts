5368de363f65db973c183fd952ffe0eb2dc0fa86
Igor Shadurin <igor.shadurin@gmail.com>
2022-04-09 18:29:43 +0300

    refactor: changed topic type to Utils.Bytes<32>

--- src/feed/handler.ts
+++ src/feed/handler.ts
@@ -12,7 +12,7 @@ const TOPIC_LENGTH = 32
  * @param level level in epoch for content
  * @returns swarm reference
  */
-export function getId(topic: Uint8Array, time = 0, level = HIGHEST_LEVEL): Utils.Bytes<32> {
+export function getId(topic: Utils.Bytes<32>, time = 0, level = HIGHEST_LEVEL): Utils.Bytes<32> {
   const bufId = new Uint8Array(40)
   let cursor = 0
   for (let i = 0; i < TOPIC_LENGTH; i++) {
