e08e000b589ac2cb3f975b9cdafcdc496f043f1c
Igor Shadurin <igor.shadurin@gmail.com>
2022-04-09 17:59:14 +0300

    refactor: upgraded description and return type for mnemonic methods

--- src/account/mnemonic.ts
+++ src/account/mnemonic.ts
@@ -11,11 +11,13 @@ import { stringToBytes } from '../utils/bytes'
  * @param bee Bee client
  * @param username FDP account username
  * @param address FDP account address
+ *
+ * @returns encrypted mnemonic phrase in Base64url format
  */
-export async function getEncryptedMnemonic(bee: Bee, username: string, address: Utils.EthAddress): Promise<Data> {
+export async function getEncryptedMnemonic(bee: Bee, username: string, address: Utils.EthAddress): Promise<string> {
   assertUsername(username)
 
-  return (await getFeedData(bee, username, address)).data.chunkContent()
+  return (await getFeedData(bee, username, address)).data.chunkContent().text()
 }
 
 /**
@@ -24,7 +26,7 @@ export async function getEncryptedMnemonic(bee: Bee, username: string, address: 
  * @param connection connection information for data uploading
  * @param wallet FDP account Ethereum wallet
  * @param username FDP username
- * @param encryptedMnemonic encrypted mnemonic to upload
+ * @param encryptedMnemonic encrypted mnemonic phrase in Base64url format
  */
 export async function uploadEncryptedMnemonic(
   connection: Connection,
