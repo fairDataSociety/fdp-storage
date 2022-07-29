c4009c7d9e742b1614d04d95cd484aea0706cd07
Igor Shadurin <igor.shadurin@gmail.com>
2022-07-29 14:27:41 +0300

    refactor: updated jsdoc

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -3,7 +3,7 @@ import { utils } from 'ethers'
 /**
  * Get Hierarchal Deterministic Wallet from private key by index
  *
- * @param privateKey private key of Ethereum wallet
+ * @param seed data for wallet creation
  * @param index wallet index
  */
 export function getWalletByIndex(seed: Uint8Array, index: number): utils.HDNode {
