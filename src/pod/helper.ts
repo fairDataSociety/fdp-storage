4d3c3a3ecfb79b12edd05c7bfa94581fb5aab1e0
Igor Shadurin <igor.shadurin@gmail.com>
2022-08-04 10:39:47 +0300

    refactor: comment moved to correct place

--- src/pod/helper.ts
+++ src/pod/helper.ts
@@ -1,14 +1,14 @@
+import { ExtendedPodInfo } from './utils'
+import { getExtendedPodsList } from './api'
+import { prepareEthAddress } from '../utils/address'
+import { AccountData } from '../account/account-data'
+
 /**
  * Gets extended information about pods using AccountData instance and pod name
  *
  * @param accountData AccountData instance
  * @param podName pod name
  */
-import { ExtendedPodInfo } from './utils'
-import { getExtendedPodsList } from './api'
-import { prepareEthAddress } from '../utils/address'
-import { AccountData } from '../account/account-data'
-
 export async function getExtendedPodsListByAccountData(
   accountData: AccountData,
   podName: string,
