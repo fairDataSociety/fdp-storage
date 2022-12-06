18a760a6ad950223b887bded7468631043a1bf04
Rogelio Morrell <molekilla@gmail.com>
2022-12-06 18:33:21 -0500

    feat: adding benchmarks

--- benchmark.ts
+++ benchmark.ts
@@ -1,6 +1,5 @@
 import { utils } from 'ethers'
-import { PrivateKeyBytes, Utils } from '@ethersphere/bee-js'
-import { removeZeroFromHex } from './src/account/utils'
+import b from 'benny'
 
 /**
  * Get Hierarchal Deterministic Wallet from seed by index
@@ -8,7 +7,7 @@ import { removeZeroFromHex } from './src/account/utils'
  * @param seed data for wallet creation
  * @param index wallet index
  */
-export function getWalletByIndex(seed: Uint8Array, index: number): utils.HDNode {
+function getWalletByIndex(seed: Uint8Array, index: number): utils.HDNode {
   const node = utils.HDNode.fromSeed(seed)
 
   return node.derivePath(`m/44'/60'/0'/0/${index}`)
@@ -19,8 +18,8 @@ export function getWalletByIndex(seed: Uint8Array, index: number): utils.HDNode 
  *
  * @param privateKey string representation of private key
  */
-export function privateKeyToBytes(privateKey: string): PrivateKeyBytes {
-  return Utils.hexToBytes(removeZeroFromHex(privateKey))
+function privateKeyToBytes(privateKey: string): any {
+  return utils.arrayify(privateKey)
 }
 
 /**
@@ -28,8 +27,8 @@ export function privateKeyToBytes(privateKey: string): PrivateKeyBytes {
  *
  * @param mnemonic mnemonic phrase
  */
-export function mnemonicToSeed(mnemonic: string): Uint8Array {
-  return Utils.hexToBytes(removeZeroFromHex(utils.mnemonicToSeed(mnemonic)))
+function mnemonicToSeed(mnemonic: string): Uint8Array {
+  return utils.arrayify(utils.mnemonicToSeed(mnemonic))
 }
 
 /**
@@ -37,8 +36,8 @@ export function mnemonicToSeed(mnemonic: string): Uint8Array {
  *
  * @param address Ethereum address for preparation
  */
-export function prepareEthAddress(address: string | Uint8Array): Utils.EthAddress {
-  return Utils.makeEthAddress(address)
+function prepareEthAddress(address: string | Uint8Array): any {
+  return utils.arrayify(address)
 }
 
 /**
@@ -46,12 +45,9 @@ export function prepareEthAddress(address: string | Uint8Array): Utils.EthAddres
  *
  * @param privateKey string representation of private key
  */
-export function preparePrivateKey(privateKey: string): PrivateKeyBytes {
-  return Utils.hexToBytes(removeZeroFromHex(privateKey))
+function preparePrivateKey(privateKey: string): any {
+  return utils.arrayify(privateKey)
 }
-
-
-import b from 'benny'
 
 b.suite(
   'Prepare address vs initial calculated variable',
@@ -62,6 +58,28 @@ b.suite(
 
   b.add('Initialized prepareEthAddress variable', () => {
     const prepareEthAddress = '0x0000000000000000000000000000000000000000'
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({ file: 'reduce', version: '1.0.0' }),
+  b.save({ file: 'reduce', format: 'chart.html' }),
+)
+
+b.suite(
+  'Create wallet vs wallet variable in memory',
+
+  b.add('Create getWalletByIndex', () => {
+    getWalletByIndex(
+      utils.arrayify(
+        `0x5eb00bbddcf069084889a8ab9155568165f5c453ccb85e70811aaed6f6da5fc19a5ac40b389cd370d086206dec8aa6c43daea6690f20ad3d8d48b2d2ce9e38e4`,
+      ),
+      0,
+    )
+  }),
+
+  b.add('Initialized wallet variable', () => {
+    const seed = '0x0000000000000000000000000000000000000000'
   }),
 
   b.cycle(),
