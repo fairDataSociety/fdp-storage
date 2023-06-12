bf60820dfa1067c39bbfa9ef21f67437f3593030
Igor Shadurin <igor.shadurin@gmail.com>
2023-06-12 20:00:16 +0300

    fix: handling error insufficient funds error

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,4 +1,4 @@
-import { CHUNK_ALREADY_EXISTS_ERROR } from '../account/utils'
+import { ERROR_CHUNK_ALREADY_EXISTS, ERROR_INSUFFICIENT_FUNDS } from '../account/utils'
 
 /**
  * Axios' error structure which comes from bee-js library
@@ -8,6 +8,15 @@ export interface AxiosError {
     data?: {
       message?: string
     }
+  }
+}
+
+/**
+ * RPC error structure which comes from `fdp-contracts-js` library
+ */
+export interface RpcError {
+  error?: {
+    message?: string
   }
 }
 
@@ -29,5 +38,14 @@ export class BeeArgumentError extends BeeError {
 export function isChunkAlreadyExistsError(e: unknown): boolean {
   const axiosError = e as AxiosError
 
-  return Boolean(axiosError?.response?.data?.message?.startsWith(CHUNK_ALREADY_EXISTS_ERROR))
+  return Boolean(axiosError?.response?.data?.message?.startsWith(ERROR_CHUNK_ALREADY_EXISTS))
+}
+
+/**
+ * Checks insufficient funds error
+ */
+export function isInsufficientFundsError(e: unknown): boolean {
+  const axiosError = e as RpcError
+
+  return Boolean(axiosError?.error?.message?.startsWith(ERROR_INSUFFICIENT_FUNDS))
 }
