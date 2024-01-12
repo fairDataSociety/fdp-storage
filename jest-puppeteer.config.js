9e59e7d1d79646a21dbd005d1b482e7c0604fc60
Igor Shadurin <igor.shadurin@gmail.com>
2024-01-12 18:26:14 +0300

    refactor: puppeteer headless

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   launch: {
     dumpio: true, // Forwards browser console into test console for easier debugging
+    headless: 'new', // Opt-in to the new headless mode for Chrome
   },
 }
