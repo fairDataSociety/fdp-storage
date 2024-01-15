7ef072785d1b18d404e175064ce3611db0fe8f83
Igor Shadurin <igor.shadurin@gmail.com>
2024-01-15 18:37:47 +0300

    feat: puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -6,5 +6,6 @@ module.exports = {
     dumpio: true, // Forwards browser console into test console for easier debugging
     headless: 'new', // Opt-in to the new headless mode for Chrome
     // executablePath: executablePath(),
+    channel: 'chrome',
   },
 }
