34e68f5d6790e270d4964a0c1ab4797f3c5e801a
Igor Shadurin <igor.shadurin@gmail.com>
2024-01-15 18:24:50 +0300

    feat: puppeteer config

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -1,5 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { join } = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { executablePath } = require('puppeteer')
+
+// eslint-disable-next-line no-console
+console.log('executablePath2', executablePath())
 
 /**
  * @type {import("puppeteer").Configuration}
