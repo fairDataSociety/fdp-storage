bce4b94093ffb816c38b93c5b64a3f7ca63ea1c1
Igor Shadurin <igor.shadurin@gmail.com>
2024-01-15 17:17:13 +0300

    feat: puppeteer config

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -1,10 +1,11 @@
+const executablePath = `/usr/bin/${process.env.PUPPETEER_EXEC_PATH}`
 // eslint-disable-next-line no-console
-console.log('process.env.PUPPETEER_EXEC_PATH', process.env.PUPPETEER_EXEC_PATH)
+console.log('process.env.PUPPETEER_EXEC_PATH', executablePath)
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
   // cacheDirectory: join(__dirname, '.cache', 'puppeteer-data'),
-  executablePath: process.env.PUPPETEER_EXEC_PATH,
+  executablePath,
 }
