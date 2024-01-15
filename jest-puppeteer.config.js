a79cc0f04183c5c790f05507dfc814fa3a18e0f2
Igor Shadurin <igor.shadurin@gmail.com>
2024-01-15 17:40:29 +0300

    feat: puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { executablePath } = require('puppeteer')
 module.exports = {
   launch: {
     dumpio: true, // Forwards browser console into test console for easier debugging
     headless: 'new', // Opt-in to the new headless mode for Chrome
+    executablePath: executablePath(),
   },
 }
--- puppeteer.config.js
+++ puppeteer.config.js
@@ -1,8 +1,8 @@
 // const executablePath = `/usr/bin/${process.env.PUPPETEER_EXEC_PATH}`
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { executablePath } = require('puppeteer')
+// const { executablePath } = require('puppeteer')
 // eslint-disable-next-line no-console
-console.log('process.env.PUPPETEER_EXEC_PATH', executablePath)
+// console.log('process.env.PUPPETEER_EXEC_PATH', executablePath)
 /**
  * @type {import("puppeteer").Configuration}
  */
@@ -10,5 +10,5 @@ module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer-data'),
   skipDownload: false,
-  executablePath: executablePath(),
+  // executablePath: executablePath(),
 }
