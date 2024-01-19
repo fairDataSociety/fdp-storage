4592e44baf5a691aae6d6bb850d867527793b8aa
Igor Shadurin <igor.shadurin@gmail.com>
2024-01-19 17:39:28 +0300

    refactor: removed comments

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,11 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { executablePath } = require('puppeteer')
-// console.log('executablePath', executablePath())
 module.exports = {
   launch: {
     dumpio: true, // Forwards browser console into test console for easier debugging
     headless: 'new', // Opt-in to the new headless mode for Chrome
-    // executablePath: executablePath(),
     channel: 'chrome',
   },
 }
